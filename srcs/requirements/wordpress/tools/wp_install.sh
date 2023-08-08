db98778f5a4bfcd41ea762fd6f9dfd7b63f1c846
Mohammed taha Meaizi <momeaizi@e1r4p4.1337.ma>
2023-08-08 18:42:05 +0100

    mariaDB's turn now

--- srcs/requirements/wordpress/tools/wp_install.sh
+++ srcs/requirements/wordpress/tools/wp_install.sh
@@ -1,7 +1,13 @@
-# cd /var/www/html
-# wget https://wordpress.org/latest.zip
-# unzip latest.zip
-# chown -R www-data:www-data /var/www/html/wordpress
+mkdir /var/www/html
+cd /var/www/html
+wget https://wordpress.org/latest.zip
+unzip latest.zip
+chown -R www-data:www-data /var/www/html/wordpress
 
-service php7.3-fpm start
+
+
+
+
+mkdir -p /run/php/
+
 php-fpm7.3 --nodaemonize
