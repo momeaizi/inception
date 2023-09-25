d46331997e87730ec98ec7b4f366584456ba4eaf
momeaizi <mohamedtahameaizi@gmail.com>
2023-09-25 15:57:23 +0100

    done

--- srcs/requirements/mariadb/tools/db_install.sh
+++ srcs/requirements/mariadb/tools/db_install.sh
@@ -1,10 +1,8 @@
-
 service mysql start
+
 mysql -e "CREATE DATABASE IF NOT EXISTS wordpress_db ;"
 mysql -e "CREATE USER IF NOT EXISTS $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
 mysql -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO $MYSQL_USER@'%' ;"
-
-echo "wordpress database and user created succesfuly!"
 
 service mysql stop
 
